c1b9223287ed3be49e2d0096fbd49d4d19d4e49b
Zach-Morris <zach-morris@lambdastudents.com>
2021-04-23 17:22:15 -0400

    Building Homepage Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
+    <div className='homePageHeader'>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Link to='/'
+        className='order-pizza' >
+        <button className='order-pizza'>Pizza?</button>
+      </Link>
+    </div>
+
+    <Switch>
+      <Route path='/'>
+        <MyForm />
+      </Route>
+      <Route>
+
+      </Route>
+    </Switch>
     </>
   );
 };
